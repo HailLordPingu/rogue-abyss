1d65801ebfbe40b52143abdf29a27ab67cc15d26
“Spectorite” <ray1357klik@gmail.com>
2023-04-20 21:41:02 -0700

    Let's see if my git knowledge and codin knowlege works out

--- src/mapgen.c
+++ src/mapgen.c
@@ -32,7 +32,7 @@ struct Chunk genRandom(int per) {
 	struct Chunk testmap;
 	for(int i = 0; i < GAME_Y; i++) { // y
 		for(int j = 0; j < GAME_X; j++) { // x
-			if(rand%100 <= per-1){
+			if(rand()%100 <= per-1){
 				testmap.tilemap[i][j] = default_tile();
 			}
 		}
